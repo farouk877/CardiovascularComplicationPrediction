39279f304ce7b8f0d75eac906bdd824e2e62cc96
mrajan31 <mrajan222@gatech.edu>
2020-10-25 16:57:43 -0400

    Added cleaning py

---
+++ readcsv.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+filename = 'risk.csv'
+
+df=pd.read_csv(filename, sep=',', header=None)
+values = df.values
+values = np.delete(values, [0, 1, 3], axis=1)
+values[:, [0, -1]] = values[:, [-1, 0]]
+header = values[0]
+values = np.delete(values, [0], axis=0)
+total_rows = values.shape[0]
+indices = np.argwhere(values.astype(float) == -1)[:,0]
+removed_rows = indices.shape[0]
+values = np.delete(values, indices, axis=0)
+values = np.append([header], values, axis=0)
+print(str(removed_rows), "/", str(total_rows), "removed")
+df = pd.DataFrame(values)
+df.to_csv('clean_risk.csv', header=None, index=False)
